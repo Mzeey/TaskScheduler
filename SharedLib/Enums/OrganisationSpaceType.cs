e083537ebd948b1d49cc0b678689ebe8ec9ead4b
Chukwudi Nwobodo <mzeeysoftware@gmail.com>
2023-07-14 20:25:04 +0100

    - Creates Interface for OrganisationSpaceService

---
+++ SharedLib/Enums/OrganisationSpaceType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mzeey.SharedLib.Enums
+{
+    public enum OrganisationSpaceType
+    {
+        [Description("Private")]
+        Private = 1,
+
+        [Description("Public")]
+        Public = 2,
+    }
+}
---
+++ TaskSchedulerLib/Services/IOrganisationSpaceService.cs
@@ -0,0 +1,24 @@
+﻿using Mzeey.Entities;
+using Mzeey.SharedLib.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSchedulerLib.Services
+{
+    public interface IOrganisationSpaceService
+    {
+        Task<OrganisationSpace> CreateOrganisationSpaceAsync(string userId, string title, OrganisationSpaceType spaceType);
+        Task<OrganisationSpace> UpdateOrganisationSpaceAsync(string spaceId, string title, string description);
+        Task<bool> DeleteOganisationSpaceAsync(string spaceId);
+        Task<string> SendInvitationAsync(string spaceId, string inviterId, string inviteeId, UserRole role, string encryptionKey);
+        Task<bool> AcceptInvitationAsync(string invitationToken, string decryptionKey);
+        Task<bool> RejectInvitationAsync(string invitationToken, string decryptionKey);
+        Task<bool> DeleteUserFromOrganisationSpaceAsync(string spaceId, string userId);
+        Task<IEnumerable<OrganisationSpace>> GetOrganisationSpaces(string userId);
+        Task<IEnumerable<OrganisationUserSpace>> GetUsersInOrganisationSpace(string organsationSpaceId);
+        Task<OrganisationUserSpace> UpdateUserRoleAsync(string spaceId, string userId, UserRole role);
+    }
+}
